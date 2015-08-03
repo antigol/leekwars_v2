e578afb5794ed87c6494dbab785c38c1e19e0de3
Mario Geiger <geiger.mario@gmail.com>
2015-08-03 11:46:26 +0200

    v0.2
    ajoute un refresh toutes les secondes

--- leekwars_tournament_warning.user.js
+++ leekwars_tournament_warning.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          [Leek Wars] Editor - Tournament warning
 // @namespace     https://github.com/antigol/leekwars_v2
-// @version       0.1
+// @version       0.2
 // @description   Ajoute un warning
 // @author        Chabrogar
 // @projectPage   https://github.com/antigol/leekwars_v2
@@ -11,19 +11,35 @@
 // @grant         none
 // ==/UserScript==
 
-LW.on('pageload', function()
+(function()
 {
-	if (LW.currentPage == 'editor')
-	{
-		var d = new Date();
-		var hour = d.getHours(); // fonctionne uniquement dans le même fuseau horraire que la france.
-		var minutes = d.getMinutes();
-		
-		if ((hour >= 11 && hour < 14) || (hour >= 17 && hour < 20) || (hour >= 20 && hour < 23)) {
-			var color = 'orange';
-			if ((minutes >= 25 && minutes <= 31) || (minutes >= 55 || minutes <= 1)) color = 'red';
-			$('<div style="background-color:'+color+'; color:black; padding:5px; style="font-weight:bold";">Warning !</div>').insertAfter('#editor-page .page-header');
-		}
+
+function warn() {
+	var d = new Date();
+	var hour = d.getHours(); // fonctionne uniquement dans le même fuseau horraire que la france.
+	var minutes = d.getMinutes();
+	var seconds = d.getSeconds();
+	
+	if ((hour >= 11 && hour < 14) || (hour >= 17 && hour < 20) || (hour >= 20 && hour < 23)) {
+        if ((minutes >= 25 && minutes <= 31) || (minutes >= 55 || minutes <= 1)) {
+            $('.tournament_warning').css('background-color', 'red');
+        } else {
+            $('.tournament_warning').css('background-color', 'orange');
+        }
+        
+        $('.tournament_warning').html('Warning ! it is '+d.toLocaleTimeString());
+	}
+}
+
+var interval = 0;
+
+LW.on('pageload', function() {
+	if (LW.currentPage == 'editor') {
+        $('<div class="tournament_warning" style="background-color:red; color:black; padding:5px; style="font-weight:bold";"></div>').insertAfter('#editor-page .page-header');
+		interval = setInterval(warn, 1000);
+	} else {
+		clearInterval(interval);
 	}
 });
 
+})();
