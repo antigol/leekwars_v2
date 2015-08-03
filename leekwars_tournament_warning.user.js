4fc060b5e3663f255410d217fef04b6aa738e3a7
Mario Geiger <geiger.mario@gmail.com>
2015-08-03 13:59:49 +0200

    v0.3

--- leekwars_tournament_warning.user.js
+++ leekwars_tournament_warning.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name          [Leek Wars] Editor - Tournament warning
+// @name          [Leek Wars] Editor - Tournament Warning
 // @namespace     https://github.com/antigol/leekwars_v2
-// @version       0.2
-// @description   Ajoute un warning
+// @version       0.3
+// @description   Ajoute un warning dans l'éditeur si un match de tournois démarre dans pas longtemps
 // @author        Chabrogar
 // @projectPage   https://github.com/antigol/leekwars_v2
 // @downloadURL   https://github.com/antigol/leekwars_v2/raw/master/leekwars_tournament_warning.user.js
@@ -13,37 +13,46 @@
 
 (function()
 {
+    var critical_times = [[12,0], [12,30], [13,0], [13,30], [14,0], [18,0], [18,30], [19,0], [19,30], [20,0], [21,0], [21,30], [22,0], [22,30], [23,0]];
 
-function warn() {
-	var d = new Date();
-	var hour = d.getHours(); // fonctionne uniquement dans le même fuseau horraire que la france.
-	var minutes = d.getMinutes();
-	var seconds = d.getSeconds();
-	
-	if ((hour >= 11 && hour < 14) || (hour >= 17 && hour < 20) || (hour >= 20 && hour < 23)) {
-        if ((minutes >= 25 && minutes <= 31) || (minutes >= 55 || minutes <= 1)) {
-            $('.tournament_warning').css('background-color', 'red');
-        } else {
-            $('.tournament_warning').css('background-color', 'orange');
+    function warn() {
+        // fonctionne uniquement dans le même fuseau horraire que la france.
+        var d = new Date();
+        var hour = d.getHours();
+        var minutes = d.getMinutes();
+        var seconds = d.getSeconds();
+        
+        var offset = -1;
+        for (var i = 0; i < critical_times.length; ++i) {
+            var x = 60 * (critical_times[i][0] - hour) + critical_times[i][1] - minutes;
+            if (x >= 0) {
+                offset = x;
+                break;
+            }
         }
         
-        $('.tournament_warning').html('Warning ! it is '+d.toLocaleTimeString());
-    } else {
-        $('.tournament_warning').html('');
+        if (offset >= 0) {
+            if (offset <= 5) $('.tournament_warning').css('background-color', 'red');
+            else $('.tournament_warning').css('background-color', 'orange');
+
+            $('.tournament_warning').show();
+            $('.tournament_warning').html('Warning ! it is '+d.toLocaleTimeString()+' next tournament\'s match in '+offset+' minutes');
+        } else {
+            $('.tournament_warning').hide();
+        }
     }
-}
 
-var interval = 0;
+    var interval = 0;
 
-LW.on('pageload', function() {
-	if (LW.currentPage == 'editor') {
-        $('<div class="tournament_warning"></div>').insertAfter('#editor-page .page-header');
-        $('.tournament_warning').css('font-weight', 'bold');
-        $('.tournament_warning').css('padding', '3px');
-		interval = setInterval(warn, 1000);
-	} else {
-		clearInterval(interval);
-	}
-});
+    LW.on('pageload', function() {
+        if (LW.currentPage == 'editor') {
+            $('<div class="tournament_warning"></div>').insertAfter('#editor-page .page-header');
+            $('.tournament_warning').css('font-weight', 'bold');
+            $('.tournament_warning').css('padding', '3px');
+            interval = setInterval(warn, 1000);
+        } else {
+            clearInterval(interval);
+        }
+    });
 
 })();
